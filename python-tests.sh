e904205bc7d8a6c52946fa4dd72cc8de6662b351
Zachary Mooney <zamo22@student.bth.se>
2024-03-21 21:18:59 +0100

    Add more documentation

--- python-tests.sh
+++ python-tests.sh
@@ -6,10 +6,12 @@ echo -e "\nPython (Flask) will now be tested.\n"
 docker compose up data-mysql --force-recreate --detach --wait
 docker compose up app-python --force-recreate --detach --wait
 
+sleep 20
 
 function test_mysql() {  
     for i in {1..10}
     do
+        echo -e "Round: ${i}"
         # docker compose up data-mysql --force-recreate --detach --wait
         # docker compose up app-python --force-recreate --detach --wait
 
@@ -36,18 +38,35 @@ function test_mysql() {
 
 file="results.csv"
 
+echo -e "Testing: python-mysql-data-aggre-1"
 test_mysql "MySQL" "Python" "python-mysql-data-aggre-1" "http://127.0.0.1:5000/aggregation-mysql" "$file"
+
+echo -e "Testing: python-mysql-app-aggre-1"
 test_mysql "MySQL" "Python" "python-mysql-app-aggre-1" "http://127.0.0.1:5000/aggregation-python" "$file"
+
+echo -e "Testing: python-mysql-data-filter-1"
 test_mysql "MySQL" "Python" "python-mysql-data-filter-1" "http://127.0.0.1:5000/filter-mysql" "$file"
+
+echo -e "python-mysql-app-filter-1"
 test_mysql "MySQL" "Python" "python-mysql-app-filter-1" "http://127.0.0.1:5000/filter-python" "$file"
+
+echo -e "Testing: python-mysql-data-sort-1"
 test_mysql "MySQL" "Python" "python-mysql-data-sort-1" "http://127.0.0.1:5000/sorting-mysql" "$file"
+
+echo -e "Testing: python-mysql-app-sort-1"
 test_mysql "MySQL" "Python" "python-mysql-app-sort-1" "http://127.0.0.1:5000/sorting-python" "$file"
+
+echo -e "Testing: python-mysql-data-procedure-1"
 test_mysql "MySQL" "Python" "python-mysql-data-procedure-1" "http://127.0.0.1:5000/procedure-mysql" "$file"
+
+echo -e "Testing: python-mysql-app-procedure-1"
 test_mysql "MySQL" "Python" "python-mysql-app-procedure-1" "http://127.0.0.1:5000/procedure-python" "$file"
+
+echo -e "Testing: python-mysql-data-adprocedure-1"
 test_mysql "MySQL" "Python" "python-mysql-data-adprocedure-1" "http://127.0.0.1:5000/advanced-procedure-mysql" "$file"
+
+echo -e "Testing: python-mysql-app-adprocedure-1"
 test_mysql "MySQL" "Python" "python-mysql-app-adprocedure-1" "http://127.0.0.1:5000/advanced-procedure-python" "$file"
-
-
 
 docker compose down app-python
 docker compose down data-mysql
